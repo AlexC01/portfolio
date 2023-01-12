3233688ab704736052297e0b5b33da6b53eeaf36
AlexC01 <acuadraq@gmail.com>
2023-01-12 15:30:16 -0600

    style: adding css config on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+			colors: {
+				babyblue: "#BBE1FA",
+				darkerblue: "#0F4C75",
+				darkcolor: "#1B262C",
+			},
+		},
+	},
+	plugins: [],
+};
