1c5c6f3b4aa3e4cfb3a31d856f43e9d728ad05bc
AlexC01 <acuadraq@gmail.com>
2023-01-16 09:02:32 -0600

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				poppins: ["Poppins", "sans-serif"],
-			},
-			colors: {
-				babyblue: "#BBE1FA",
-				darkerblue: "#0F4C75",
-				darkcolor: "#1B262C",
-			},
-		},
-	},
-	plugins: [],
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        babyblue: '#BBE1FA',
+        darkerblue: '#0F4C75',
+        darkcolor: '#1B262C',
+        colorlight: '#E8F0F2',
+      },
+    },
+  },
+  plugins: [],
 };
